8838e1b12bcfdf731171bf9c96b8a653dc837632
Matt <mjd014@live.co.uk>
2022-09-06 18:49:08 +0000

    added queries, collecting data from our table in Chinook_PostgreSql.sql file.

--- sql-orm.py
+++ sql-orm.py
@@ -48,3 +48,41 @@ session = Session()
 
 # Creating the database using declarative_base subclass
 base.metadata.create_all(db)
+
+# Query 1 - Select all records from the "Artist" table
+# Prints ArtistId, Name seperated by a pipe " | "
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 2 - Select only the "Name" column from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.Name, sep=" | ")
+
+# # Query 3 - Select only "Queen" from the "Artist" table
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 4 - Select only by "ArtistId" #51 from the "Artist" table
+# artist = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# # Query 5 - Select only the albums with "ArtistId" #51 on the "Album" table
+# albums = session.query(Album).filter_by(ArtistId=51)
+# for album in albums:
+#     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
+
+# Query 6 -
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(track.TrackId,
+    track.Name,
+    track.AlbumId,
+    track.MediaTypeId,
+    track.GenreId,
+    track.Composer,
+    track.Milliseconds,
+    track.Bytes,
+    track.UnitPrice,
+    sep=" | ")
