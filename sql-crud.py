943cd62fbb05afbda0a49aef9a1fb67ad9e08ab6
Matt <mjd014@live.co.uk>
2022-09-06 20:00:18 +0000

    created programmers objects and added them to our new database, Programmer.

--- sql-crud.py
+++ sql-crud.py
@@ -39,7 +39,76 @@ ada_lovelace = Programmer(
     famous_for="First programmer"
 )
 
-# Add each instance of our programmers to our session
-session.add(ada_lovelace)
+allan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
+)
 
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL Language"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
+
+tim_berners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
+matt_dunks = Programmer(
+    first_name="Matt",
+    last_name="Dunkerton",
+    gender="M",
+    nationality="British",
+    famous_for="Fingerstyle Guitar"
+)
+
+# Add each instance of our programmers to our session (Similar to git add)
+# Once the programme has been run, comment out the sessions added,
+# so no duplicates
+# session.add(ada_lovelace)
+# session.add(allan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+session.add(matt_dunks)
+
+# Commit our session to the database. (Similar to git commit)
 session.commit()
+
+# Query the database to find all programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
